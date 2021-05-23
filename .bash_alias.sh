674e423e83cd51b32e20aacbc52255478b504bd5
yenonn <yenonn@gmail.com>
2021-05-23 09:53:22 +0800

    adding aliases

---
+++ .bash_alias.sh
@@ -0,0 +1,9 @@
+## Alias section 
+alias cp="cp -i"                                                # Confirm before overwriting something
+alias df='df -h'                                                # Human-readable sizes
+alias free='free -m'                                            # Show sizes in MB
+alias gitu='git add . && git commit && git push'
+alias go='/usr/local/go/bin/go'
+alias lg='lazygit'
+alias vi='vim'
+alias k9s='k9s --logoless'
