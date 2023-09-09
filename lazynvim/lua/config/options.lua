32bbdc118bad48ab0803b0097bcf94ae932eee5c
yenonn <yenonn@gmail.com>
2023-09-09 23:34:13 +0800

    config

---
+++ lazynvim/lua/config/options.lua
@@ -0,0 +1,18 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+opt.noswapfile = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.nu = true
+opt.rnu = true
+opt.nowrap = true
+opt.smartcase = true
+opt.noswapfile = true
+opt.nobackup = true
+opt.incsearch = true
+opt.splitright = true
